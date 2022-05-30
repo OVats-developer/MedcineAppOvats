24e86ec79f30f03a60fd2e15a2c5f7b563652966
Oshin Vats <oshin@edsat.org>
2022-05-30 13:46:01 +0400

    Initial Commit

---
+++ MedcineAppOvats WatchKit Extension/MedcineAppOvatsApp.swift
@@ -0,0 +1,19 @@
+//
+//  MedcineAppOvatsApp.swift
+//  MedcineAppOvats WatchKit Extension
+//
+//  Created by Oshin Vats on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MedcineAppOvatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ MedcineAppOvats/MedcineAppOvatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedcineAppOvatsApp.swift
+//  MedcineAppOvats
+//
+//  Created by Oshin Vats on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MedcineAppOvatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
